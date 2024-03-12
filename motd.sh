51e1588f6ad1389076246f205eab676e29432f9d
Anthony Arnoux <34415538+anthony-arnoux@users.noreply.github.com>
2024-03-12 16:14:42 +0100

    Update motd.sh

--- motd.sh
+++ motd.sh
@@ -2,33 +2,38 @@
 cat > /etc/update-motd.d/10-uname <<'EOF'
 #!/bin/bash
 
-# y'a internet ou pas + ip + ping
-pingoutput=$(ping -qn4c1 1.1.1.1 2>&1 | tail -n 1)
-latency=$(echo "$pingoutput" | awk -F'/' '{ print $6 }')
-if [[ -n "$latency" ]]; then
-  latency=$(echo "$latency" | cut -d"." -f1)
+# y'a internet ou pas
+if $(ping -qn4c1 1.1.1.1 >/dev/null); then
   internet="1"
-  if which curl &> /dev/null; then
-    ipext=$(curl -4 -sSL 'ifconfig.me')
-  elif which wget &> /dev/null; then
-    ipext=$(wget --inet4-only -qO- 'ifconfig.me')
-  fi
-  ptr=$(host -t PTR ${ipext} | awk {'print $NF'})
-else
-  internet="0"
 fi
 
 # DEBIAN VERSION & CO
 hostname=$(hostname)
-distrib=$(lsb_release -s -d | tail)
+distrib=$(grep 'PRETTY_NAME' /etc/os-release | cut -d '"' -f 2)
 kernel=$(uname -r)
 uptime=$(uptime -p)
 ip=$(hostname -I | cut -d " " -f1)
-deb_ver=$(cat /etc/debian_version)
+deb_ver=$(cat /etc/debian_version &> /dev/null)
 datetime=$(date "+%d/%m/%Y - %H:%M:%S")
 
 if which figlet &> /dev/null; then
   hostname_figlet=$(figlet $(hostname))
+fi
+
+# VM
+virt_type=$(systemd-detect-virt 2>/dev/null)
+if [[ -z "$virt_type" ]]; then
+  virt_type="" # no
+else
+  virt_type=" VM : \xE2\x9C\x94"
+fi
+
+# VIRT
+cpu_virt=$(cat /proc/cpuinfo | grep 'vmx\|svm')
+if [[ -z "$cpu_virt" ]]; then
+  cpu_virt="" # no
+else
+  cpu_virt=" virt tech : \xE2\x9C\x94"
 fi
 
 # CPU
@@ -77,15 +82,29 @@ diskfreepercent=$(( 100 - $diskusedpercent ))
 # Récupérer le loadavg
 read one five fifteen rest < /proc/loadavg
 
+# Pour récuperer l'ipv4 externe -> curl ou wget
+if [[ $internet = "1" ]]; then
+  if which curl &> /dev/null; then
+    ipext=$(curl -4 -sSL 'ifconfig.me')
+  elif which wget &> /dev/null; then
+    ipext=$(wget --inet4-only -qO- 'ifconfig.me')
+  fi
+  ptr=$(host -t PTR ${ipext} | awk {'print $NF'})
+fi
+
+if [[ ! -z $ipext ]]; then
+  ipext="${ipext} ✔ "
+fi
+
 # Affichage
 echo ""
 echo -e "  Nom d'hôte   \e[33m:\e[0m $hostname"
 echo -e "  Date/Heure   \e[33m:\e[0m $datetime \e[34m█\e[0m\e[37m█\e[0m\e[31m█\e[0m"
 echo -e "  Distribution \e[33m:\e[0m $distrib ($deb_ver)"
 echo -e "  Kernel       \e[33m:\e[0m $kernel"
-echo -e "  CPU          \e[33m:\e[0m $cpu_model_number"
+echo -e "  CPU          \e[33m:\e[0m $cpu_model_number $virttech $virt_type $cpu_virt"
 echo -e "  Charge CPU   \e[33m:\e[0m $one (1min) / $five (5min) / $fifteen (15min)"
-echo -e "  Adresse IP   \e[33m:\e[0m $ip | $ipext | $ptr | $latency ms"
+echo -e "  Adresse IP   \e[33m:\e[0m $ip | $ipext | $ptr"
 echo -e "  RAM          \e[33m:\e[0m $ramusedraw$unitname/$ramtot$unitname ($ramusedrawpercent%) | Total (Cache/Buffers/Bata..) : $ramused$unitname/$(($memtotal/$unit))$unitname ($ramusedpercent%) | Swap ($swappercent%)"
 echo -e "  Uptime       \e[33m:\e[0m $uptime"
 echo -e "  Disque       \e[33m:\e[0m $diskused/$disktotal ($diskusedpercent%) | Libre : $diskfree ($diskfreepercent%)"
