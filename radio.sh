4dcfdcaf7ad823ffd1753879cde0df6a7b145dcc
Anthony Arnoux <34415538+anthony-arnoux@users.noreply.github.com>
2024-03-14 15:32:23 +0100

    Update radio.sh

--- radio.sh
+++ radio.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-if $(ping -qn4c1 one.one.one.one >/dev/null); then
-  internet="1"
+if eval "ping -qn4c1 one.one.one.one >/dev/null"; then
+  true
 else
   echo "l'network il est capout"
   exit 1
 fi
 
-if [[ ! -z $1 ]] ; then
+if [[ ! -z $1 && ! -z $2 ]]; then
   station=$1
+  if [[ $2 =~ ^[0-9]{2}:[0-9]{2}$ ]]; then # format 12:34 H:M
+    stop_time=$2
+  else
+    echo "format hh:mm"
+    exit 4
+  fi
   case $station in
     nostalgie)
       station_uuid="960bf492-0601-11e8-ae97-52543be04c81"
@@ -17,38 +23,45 @@ if [[ ! -z $1 ]] ; then
       station_uuid="db97789f-ad2e-11e8-aa67-52543be04c81"
       ;;
     *)
-      echo "non supporté"
+      echo "station \"$1\" non supporté"
       exit 2
       ;;
    esac
-
 else
-  echo "pas de parametre 1"
+  echo "pas de parametre 1 et/ou 2"
   exit 3
 fi
 
-out_file="radio_$(date +"%d-%m-%Y").mp3"
-
-start_time="13:16:00"
-stop_time="13:17:00"
+out_file="radio_$(date +"%d-%m-%Y-%S").mp3"
 
 url=$(curl -fsSL "http://de1.api.radio-browser.info/json/stations/byuuid/${station_uuid}" | tr ',' '\n' | grep "url_resolved" | awk -F'"' '{print $4}')
-echo $url
-# check si dans le temps
-current_time=$(date +"%H:%M:%S")
-if [[ "$current_time" > "$start_time" && "$current_time" < "$stop_time" ]]; then
-    # ffmpeg en arriere plan
-    ffmpeg -i "$url" -c copy "$out_file" &
-    # recup le pid du process ffmpeg
-    pid=$!
 
-    # toutes les minutes, check si faut tjrs enregistrer
-    while [[ $(date +"%H:%M:%S") < "$stop_time" ]]; do
+# check si dans le temps
+curr_time=$(date +"%H:%M")
+
+ffmpeg -i "$url" -c copy "$out_file" &
+pid=$!
+
+if [[ "$curr_time" < "$stop_time" ]]; then
+    # ffmpeg en arriere plan
+    #ffmpeg -i "$url" -c copy "$out_file" &
+    # recup le pid du process ffmpeg
+    #pid=$!
+
+    # si le meme jour, toutes les minutes, check si faut tjrs enregistrer
+    while [[ $(date +"%H:%M") < "$stop_time" ]]; do
+        sleep 1m
+    done
+else
+    # si la fin est le jour d apres -> super
+    while [[ $(date +"%H:%M") < "23:59" ]]; do
         sleep 1m
     done
 
+    while [[ $(date +"%H:%M") < "$stop_time" ]]; do
+        sleep 1m
+    done
     # kill propre du ffmpeg
-    kill -SIGINT $pid
-else
-    echo "hors des temps, ignore"
+    #kill -SIGINT $pid
 fi
+kill -SIGINT $pid
