b1f679355236df2fcf6a19df46ea0ab19d936f71
Anthony Arnoux <34415538+anthony-arnoux@users.noreply.github.com>
2024-03-15 14:47:24 +0100

    Update radio.sh

--- radio.sh
+++ radio.sh
@@ -44,7 +44,7 @@ else
   exit 3
 fi
 
-out_file='${station}_$(date +"%Y-%d-%m_%H-%M")'
+out_file="${station}_$(date +"%Y-%d-%m_%H-%M")"
 
 url=$(curl -fsSL "http://de1.api.radio-browser.info/json/stations/byuuid/${station_uuid}" | tr ',' '\n' | grep "url_resolved" | awk -F'"' '{print $4}')
 
