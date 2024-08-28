7a237785d57c74c59474c3a4574d87ae53844538
Anthony Arnoux <34415538+anthony-arnoux@users.noreply.github.com>
2024-08-28 10:23:52 +0200

    Update motd-dl.sh

--- motd-dl.sh
+++ motd-dl.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
 sleep 5
-curl -fsSL "https://raw.githubusercontent.com/anthony-arnoux/trucs/main/motd.sh" | bash
+
+if which curl &> /dev/null; then
+    script_content=$(curl -4 -fsSL 'https://raw.githubusercontent.com/anthony-arnoux/trucs/main/motd.sh')
+elif which wget &> /dev/null; then
+    script_content=$(wget --inet4-only -qO- 'https://raw.githubusercontent.com/anthony-arnoux/trucs/main/motd.sh')
+fi
+
+echo "$script_content" | bash
 exit
