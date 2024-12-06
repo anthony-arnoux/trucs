03c93e67bb26d9b3879c4c755cd3f36ef3608aca
Anthony Arnoux <34415538+anthony-arnoux@users.noreply.github.com>
2024-12-06 10:38:06 +0100

    Update motd-dl.sh - remove stupid sleep

--- motd-dl.sh
+++ motd-dl.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sleep 5
-
 if which curl &> /dev/null; then
     script_content=$(curl -4 -fsSL 'https://raw.githubusercontent.com/anthony-arnoux/trucs/main/motd.sh')
 elif which wget &> /dev/null; then
