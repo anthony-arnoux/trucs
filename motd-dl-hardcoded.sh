82e19f87e93716bf13760b5f5ae8dabba4a0248e
Anthony Arnoux <34415538+anthony-arnoux@users.noreply.github.com>
2025-08-01 11:28:41 +0200

    Update motd-dl-hardcoded.sh

--- motd-dl-hardcoded.sh
+++ motd-dl-hardcoded.sh
@@ -3,9 +3,9 @@
 sleep 5
 
 if which curl &> /dev/null; then
-    script_content=$(curl -4 -fsSL 'https://fart.ovh/motd')
+    script_content=$(curl -4 -fsSL 'https://motd.fart.ovh')
 elif which wget &> /dev/null; then
-    script_content=$(wget --inet4-only -qO- 'https://fart.ovh/motd')
+    script_content=$(wget --inet4-only -qO- 'https://motd.fart.ovh')
 fi
 
 echo "$script_content" | bash
